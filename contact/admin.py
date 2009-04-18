6b087095db7bcddfb160a511c24c4ac51d047dfa
tommy.stranded@gmail.com <tommy.stranded@gmail.com@99b1080a-27be-11de-a0b0-8331912bf998>
2009-04-18 19:04:07 +0000

    Added new models for IM, MicroBlog and URL's as well as making all the supplementary models inherited from CanBePrimary

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from ..contact.supplemental import PhoneNumber, EmailAddress
+from ..contact.supplemental import PhoneNumber, EmailAddress, IMAddress, MicroBlog, URLAddress
 import django.forms as forms
 
 admin.site.register(PhoneNumber)
 admin.site.register(EmailAddress)
+admin.site.register(IMAddress)
+admin.site.register(MicroBlog)
+admin.site.register(URLAddress)
--- contact/supplemental.py
+++ contact/supplemental.py
@@ -3,18 +3,23 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.utils.translation import ugettext_lazy as _
 from ..geographic.country import Country
 import re
-from ..content.content import ContentModel
+from ..content.content import CanBePrimary
 
-class EmailAddress(ContentModel):
-  emailaddr = models.EmailField()  #contactinfo = models.ForeignKey(ContactDetails)
+class EmailAddress(CanBePrimary):
+  emailtype = models.CharField(max_length=6, choices=(('PUBLIC',_('Public')),('PRIVATE',_('Private'))), default='PRIVATE' )
+  emailaddr = models.EmailField() 
+  class Meta(CanBePrimary.Meta):
+    verbose_name_plural = _("Email Addresses")
+
   def __unicode__(self):
     return self.emailaddr
   
-class PhoneNumber(ContentModel):
+class PhoneNumber(CanBePrimary):
   phonenumbertype = models.CharField(max_length=6, choices=(('FAX', _('Fax')), ('WORK', _('Work')),('OFFICE',_('Office')),('MOBILE',_('Mobile')),('DIRECT',_('Direct')),('HOME',_('Home')),))
   country = models.ForeignKey(Country)
   phonenumber = models.PositiveIntegerField()
-  comment = models.CharField(max_length=20,null=True)
+  class Meta(CanBePrimary.Meta):
+    verbose_name_plural = _("Phone Numbers")
 
   def __unicode__(self):
     return self.formated_phone_number()
@@ -22,3 +27,63 @@ class PhoneNumber(ContentModel):
   def formated_phone_number(self, format_index=0):
     return self.county.formated_phone_number(self.phonenumber, format_index)
   
+class IMAddress(CanBePrimary):
+  imchoices = ( # Types taken from http://en.wikipedia.org/wiki/Instant_messaging
+                ('AIM',_('AOL Instant Messenger')),
+                ('EBUDDY',_('eBuddy')),
+                ('GADUGADU',_('Gadu-Gadu')),
+                ('SAMETIME',_('IBM Lotus Sametime')),
+                ('ICQ',_('ICQ')),
+                ('IMVU',_('IMVU')),
+                ('XMPP',_('Jabber')),
+                ('GOOGLETALK',_('Google Talk')),
+                ('MRA',_('Mail.ru Agent')),
+                ('MEEBO',_('Meebo')),
+                ('MUNDU',_('Mundu Messenger')),
+                ('MXIT',_('MXit')),
+                ('PALTALK',_('Paltalk')),
+                ('PSYC',_('PSYC')),
+                ('SKYPE',_('Skype')),
+                ('TENCENTQQ',_('Tencent QQ')),
+                ('VZO',_('VZOchat')),
+                ('MSN',_('Windows Live Messenger (MSN)')),
+                ('XFIRE',_('Xfire')),
+                ('YAHOO',_('Yahoo! Messenger')),
+              )
+  imtype = models.CharField(max_length=15, choices=imchoices )
+  account = models.CharField(max_length=200)
+  class Meta(CanBePrimary.Meta):
+    verbose_name_plural = _("IM Addresses")
+
+  def __unicode__(self):
+    return "IM [%s] %s" % (self.get_imtype_display(), self.account)
+
+class MicroBlog(CanBePrimary):
+  microblogchoices = (
+    ('TWITTER',_('Twitter')),
+    ('POWNCE',_('Pownce')),
+    ('JAIKU',_('Jaiku')),
+  )
+  microblogtype = models.CharField(max_length=15, choices=microblogchoices)
+  address = models.CharField(max_length=200)
+  class Meta(CanBePrimary.Meta):
+    verbose_name_plural = _("Micro Blogs")
+
+  def __unicode__(self):
+    return u"Micro Blog [%s] %s" % (self.get_microblogtype_display(), self.address)
+
+class URLAddress(CanBePrimary):
+  urlchoices = (
+    ('CORPORATE',_('Corporate Website')),
+    ('BLOG',_('Personal Blog')),
+    ('OTHER',_('Other Website')),
+  )
+  urltype = models.CharField(max_length=15, choices=urlchoices, default='OTHER')
+  url = models.URLField()
+  class Meta(CanBePrimary.Meta):
+    verbose_name = _("URL Address")
+    verbose_name_plural = _("URL Addresses")
+  
+  def __unicode__(self):
+    return u"URL [%s] %s" % (self.get_urltype_display(), self.url)
+
